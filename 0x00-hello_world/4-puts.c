8e723098dba2b0e35d7fccdf8b63ff8fbabc39aa
humphrey <humphrey.nyahoja02@gmail.com>
2023-07-14 03:44:26 -0700

    gogo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
-int main() {
-	puts("\"Programming is like building a multilingual puzzel");
-	return 0;
+#include<stdio.h
+/* this function prints without using printf
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
