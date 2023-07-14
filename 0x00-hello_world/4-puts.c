0ec795259672034ee999376f590909c6b4309eb4
humphrey <humphrey.nyahoja02@gmail.com>
2023-07-14 03:52:46 -0700

    hoyo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-/* this function prints without using printf
+/* 
+ * this function prints without using printf
  */
 int main(void)
 {
+	/*this one is the function*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
